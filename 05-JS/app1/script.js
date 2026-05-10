776f3ee5b293e6f92721f04214ac82e030c00a43
vishwas singh <vishwasks.reach@gmail.com>
2026-05-10 09:43:45 +0530

    Basics of JavaScript

---
+++ 05-JS/app1/script.js
@@ -0,0 +1,19 @@
+var nme = "vishwas";
+console.log(`My Name is: ${nme}`);
+let age = 1000
+
+
+{
+    let age = 100
+    console.log("I am inside a block")
+    console.log(age)
+    console.log(`My Name is: ${nme}`);
+}
+
+
+console.log("I am outside the block")
+console.log(`My Name is: ${nme}`);
+console.log(age)
+
+
+
